a31a2ac1f8531dc1a213aa21263e290d8275b08f
Jesaiah Prayor <jesaiahprayor@gmail.com>
2018-01-09 16:00:48 -0500

    took out email vertification

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const email = require ('./email_vertification.js')
 const database = require('./database') // database..
 const app = express();
 
