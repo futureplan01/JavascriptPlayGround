f2c94c6608a3a1b8bc0fe272f91b4fd1a00c3d2f
Jesaiah Prayor <jesaiahprayor@gmail.com>
2018-09-04 21:55:17 -0400

    fixing chat display

--- react-client/src/Components/MyComponents/ChatApp/ChatScreen.jsx
+++ react-client/src/Components/MyComponents/ChatApp/ChatScreen.jsx
@@ -13,7 +13,10 @@ class ChatScreen extends Component {
           console.log(data);
           this.state.messages.push(data);
           let name = false;
+          
           if (this.props.name === data.user) name = true;
+          console.log('I am : ' + this.props.name);
+          console.log("I got message from : " + data.user);
           this.setState({ message: data, fromMe: name});
       });
 
@@ -23,12 +26,12 @@ class ChatScreen extends Component {
       const fromMe = this.props.fromMe ? "fromMe" : "";
       return <div className="chatScreen">
           {this.state.messages.map(data => { 
-              return 
-              <div className='messages' id= {fromMe} >
-                <h3 className = 'chat' key={data.user + ":" + data.index}>
+              return (
+              <div className='messages' >
+                  <h3 className='chat' id={fromMe} key={data.user + ":" + data.index}>
                   {data.user + ": " + data.message}
-                </h3>;
-              </div>
+                </h3>
+              </div>);
             })}
         </div>;
   }
